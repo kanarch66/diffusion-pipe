cf7b11719f6daf9b86b3714faeaca53eaec92e0b
Kanto <66814471+kanarch66@users.noreply.github.com>
2025-01-02 11:06:16 +0700

    Update diffusion-pipe-install.sh

--- diffusion-pipe-install.sh
+++ diffusion-pipe-install.sh
@@ -76,6 +76,6 @@ cd /workspace/ComfyUI/models/clip/clip-vit-large-patch14
 for file in /home/user/diffusion-pipe/models/clip/*; do
     ln -s "$file" "$(pwd)/$(basename "$file")"
 done
-ln -s /home/user/diffusion-pipe/models/hunyuan/hunyuan_video_vae_bf16.safetensors /workspace/ComfyUI/models/vae
-ln -s /home/user/diffusion-pipe/models/hunyuan/hunyuan_video_720_cfgdistill_fp8_e4m3fn.safetensors /workspace/ComfyUI/models/diffusion_models/Hunyuan
+ln -s /home/user/diffusion-pipe/models/hunyuan/hunyuan_video_vae_bf16.safetensors /workspace/ComfyUI/models/vae/
+ln -s /home/user/diffusion-pipe/models/hunyuan/hunyuan_video_720_cfgdistill_fp8_e4m3fn.safetensors /workspace/ComfyUI/models/diffusion_models/Hunyuan/
 
